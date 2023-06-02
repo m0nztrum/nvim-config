398b62735e24f22608920831ca6c99788536e984
Dragonixj <delafiado@gmail.com>
2023-06-02 17:39:11 +0000

    removed some useless plugins

--- lua/plugin_config/lualine.lua
+++ lua/plugin_config/lualine.lua
@@ -4,10 +4,8 @@
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		-- component_separators = { left = "", right = "" },
-		-- section_separators = { left = "", right = "" },
-		component_separators = { left = "|", right = "|" },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
@@ -25,7 +23,10 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = { { "filename" ,
+        file_status = true,
+        path = 0
+    } },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },

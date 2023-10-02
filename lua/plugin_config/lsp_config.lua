9c87bf518c7a30c2d4277ebd3a751b245e6ee4d3
Dragonixj <delafiado@gmail.com>
2023-10-02 19:48:39 +0000

    reversed to my old stuff

---
+++ lua/plugin_config/lsp_config.lua
@@ -0,0 +1,40 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").lua_ls.setup({
+	capabilities = capabilities,
+})
+require("lspconfig").rust_analyzer.setup({
+
+	capabilities = capabilities,
+})
+
+require("lspconfig").clangd.setup({
+
+	capabilities = capabilities,
+})
+
+require("lspconfig").cssls.setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig").tsserver.setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig").html.setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig").pylsp.setup({
+
+	capabilities = capabilities,
+})
+
+require("lspconfig").bashls.setup({
+	capabilities = capabilities,
+})

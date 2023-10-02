27ea2739b93484a83af5aadbefface049c4e141e
Dragonixj <delafiado@gmail.com>
2023-10-02 11:45:15 +0000

    overhall

--- lua/plugin_config/lualine.lua
+++ lua/plugin_config/lualine.lua
@@ -58,7 +58,7 @@ require("lualine").setup({
 	winbar = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { { "filename", path = 2 } },
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = {},
 		lualine_y = {},
 		lualine_z = {},

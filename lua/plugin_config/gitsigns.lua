76c2543b195c014ca9d6da79671d554e6678c9a4
Dragonixj <delafiado@gmail.com>
2023-10-03 12:05:41 +0000

    tweeked gitsigns and removed some stuff from lualine(winbar)

--- lua/plugin_config/gitsigns.lua
+++ lua/plugin_config/gitsigns.lua
@@ -1,5 +1,6 @@
-require('gitsigns').setup({
-    signs = {
-        untracked = {text = '|'},
-    },
-})
+local status, gitsigns = pcall(require, "gitsigns")
+if not status then
+	return
+end
+
+gitsigns.setup({})

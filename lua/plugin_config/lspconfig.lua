0b46ac9b96eb358492a4f8925076499059ed4014
Dragonixj <delafiado@gmail.com>
2023-10-04 12:06:10 +0000

    made some changes

--- lua/plugin_config/lspconfig.lua
+++ lua/plugin_config/lspconfig.lua
@@ -46,3 +46,10 @@ nvim_lsp.pylsp.setup({
 nvim_lsp.tsserver.setup({
 	capabilities = capabilities,
 })
+
+-- Diagnostic symbols in the sign column (gutter)
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end

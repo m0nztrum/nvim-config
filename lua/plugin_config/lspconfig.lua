bfb10652e5b2f90b38f267e581262f33e4df1a8d
Dragonixj <delafiado@gmail.com>
2023-10-04 13:01:20 +0000

    made some changes

--- lua/plugin_config/lspconfig.lua
+++ lua/plugin_config/lspconfig.lua
@@ -2,10 +2,10 @@ local status, nvim_lsp = pcall(require, "lspconfig")
 if not status then
 	return
 end
-
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+--  server configurations
 --      [Lua]
 nvim_lsp.lua_ls.setup({
 	capabilities = capabilities,

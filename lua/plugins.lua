50d08736ba94f92a4c5b7f6295b8eac54edd5723
Dragonixj <delafiado@gmail.com>
2023-10-13 11:23:48 +0000

    migrated to lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,152 +1,141 @@
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-
-	-- MY PLUGINS --
-
+local plugins = {
 	--          [Theme]
-	use("ellisonleao/gruvbox.nvim")
-	use("water-sucks/darkrose.nvim")
-	use("EdenEast/nightfox.nvim")
-	use({ "rose-pine/neovim", as = "rose-pine" })
-	use("catppuccin/nvim")
-	use("davidosomething/vim-colors-meh")
-	use("rebelot/kanagawa.nvim")
-	use("projekt0n/github-nvim-theme")
+	"ellisonleao/gruvbox.nvim",
+	"water-sucks/darkrose.nvim",
+	"EdenEast/nightfox.nvim",
+	{ "rose-pine/neovim", name = "rose-pine" },
+	"catppuccin/nvim",
+	"davidosomething/vim-colors-meh",
+	"rebelot/kanagawa.nvim",
+	"projekt0n/github-nvim-theme",
 
 	--        [nvim tree]
-	use("nvim-tree/nvim-tree.lua")
+	"nvim-tree/nvim-tree.lua",
 
 	--        [Undo Tree]
-	use("mbbill/undotree")
+	"mbbill/undotree",
 
 	--      [Web devicons]
-	use("nvim-tree/nvim-web-devicons")
+	"nvim-tree/nvim-web-devicons",
 
 	--      [treesitter]
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-
+	{ { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" } },
 	--lua line
-	use("nvim-lualine/lualine.nvim")
+	"nvim-lualine/lualine.nvim",
 
 	-- Telescope
-	use({
+	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.3",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+		version = "0.1.3",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
 	--      [Lsp signature]
-	use("ray-x/lsp_signature.nvim")
+	"ray-x/lsp_signature.nvim",
 
 	--          [LSP]
 	-- LSP Support
-	use("neovim/nvim-lspconfig") -- Required
-	use("williamboman/mason.nvim") -- Optional
-	use({ "williamboman/mason-lspconfig.nvim" }) -- Optional
-	use("whoIsSethDaniel/mason-tool-installer.nvim")
+	"neovim/nvim-lspconfig", -- Required
+	"williamboman/mason.nvim", -- Optional
+	{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+	"whoIsSethDaniel/mason-tool-installer.nvim",
 	-- Autocompletion
-	use("hrsh7th/nvim-cmp") -- Required
-	use("hrsh7th/cmp-nvim-lsp") -- Required
-	use("hrsh7th/cmp-buffer") -- Optional
-	use("hrsh7th/cmp-path") -- Optional
-	use("saadparwaiz1/cmp_luasnip") -- Optional
-	use("hrsh7th/cmp-nvim-lua") -- Optional
+	"hrsh7th/nvim-cmp", -- Required
+	"hrsh7th/cmp-nvim-lsp", -- Required
+	"hrsh7th/cmp-buffer", -- Optional
+	"hrsh7th/cmp-path", -- Optional
+	"saadparwaiz1/cmp_luasnip", -- Optional
+	"hrsh7th/cmp-nvim-lua", -- Optional
 	-- Snippets
-	use("L3MON4D3/LuaSnip") -- Required
-	use("rafamadriz/friendly-snippets") -- Optional
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("onsails/lspkind-nvim") -- vscode like pictograms
+	"L3MON4D3/LuaSnip", -- Required
+	"rafamadriz/friendly-snippets", -- Optional
+	"jose-elias-alvarez/null-ls.nvim",
+	"onsails/lspkind-nvim", -- vscode like pictograms
 
-	use("nvimdev/lspsaga.nvim") -- LSP UIs
+	"nvimdev/lspsaga.nvim", -- LSP UIs
 
 	--          [autopair]
-	use("windwp/nvim-autopairs")
+	"windwp/nvim-autopairs",
 
 	--          [Git]
-	use("lewis6991/gitsigns.nvim") -- gitsigns
-	use("airblade/vim-gitgutter") -- gitgutter
-	use("tpope/vim-fugitive") -- fugitive
-	use("tpope/vim-rhubarb")
+	"lewis6991/gitsigns.nvim", -- gitsigns
+	"airblade/vim-gitgutter", -- gitgutter
+	"tpope/vim-fugitive", -- fugitive
+	"tpope/vim-rhubarb",
 
 	--       [Markdown]
-	use({
+	{
 		"iamcco/markdown-preview.nvim",
-		run = function()
+		build = function()
 			vim.fn["mkdp#util#install"]()
 		end,
-	})
+	},
 
 	--      [vim cool]
-	use("romainl/vim-cool")
+	"romainl/vim-cool",
 
 	--      [Live-server]
-	-- use("manzeloth/live-server")
-	use("norcalli/nvim-colorizer.lua")
+	-- "manzeloth/live-server"
+
+	--      [colorizer]
+	"norcalli/nvim-colorizer.lua",
 
 	--      [Transparent - allows for toggle]
-	use("xiyaowong/nvim-transparent")
+	"xiyaowong/nvim-transparent",
 
 	--      [diffview]
-	use("sindrets/diffview.nvim")
+	"sindrets/diffview.nvim",
 
 	--          [barbar]
-	use("romgrk/barbar.nvim")
+	"romgrk/barbar.nvim",
 
 	--          [Vgit]
-	use({
+	{
 		"tanvirtin/vgit.nvim",
-		requires = {
+		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-	})
+	},
 	--  [Terminal in the floating/popup window.]
-	use("akinsho/toggleterm.nvim")
+	"akinsho/toggleterm.nvim",
 
 	--      [Wilder]
-	use("gelguy/wilder.nvim")
+	"gelguy/wilder.nvim",
 
 	--      [Smoothie]
-	use("psliwka/vim-smoothie")
+	"psliwka/vim-smoothie",
 
 	--  [Add indentation guides on even blank lines]
-	use("lukas-reineke/indent-blankline.nvim")
+	"lukas-reineke/indent-blankline.nvim",
 
 	--      [Notify]
-	use("rcarriga/nvim-notify")
+	"rcarriga/nvim-notify",
 
 	--      [Whick key]
-	use({
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-	})
+	{ "folke/which-key.nvim" },
 
 	--      [commentry]
-	use("tpope/vim-commentary")
+	"tpope/vim-commentary",
 
 	--      [Vim Tex]
-	use("lervag/vimtex")
+	"lervag/vimtex",
 
 	--      [zen]
-	use("folke/zen-mode.nvim")
+	"folke/zen-mode.nvim",
+}
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
+local opts = {}
+
+require("lazy").setup(plugins, opts)

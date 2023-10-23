16c66727eacda3e3de8acebd8a14208ad7fd4fc6
Dragonixj <delafiado@gmail.com>
2023-10-23 01:38:39 +0000

    better diagnostics

--- lua/configs/barbar.lua
+++ lua/configs/barbar.lua
@@ -29,10 +29,10 @@ bar.setup({
 		button = "",
 		-- Enables / disables diagnostic symbols
 		diagnostics = {
-			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "" },
-			[vim.diagnostic.severity.WARN] = { enabled = true },
-			[vim.diagnostic.severity.INFO] = { enabled = true },
-			[vim.diagnostic.severity.HINT] = { enabled = true },
+			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
+			[vim.diagnostic.severity.WARN] = { enabled = true, icon = " " },
+			[vim.diagnostic.severity.INFO] = { enabled = true, icon = " " },
+			[vim.diagnostic.severity.HINT] = { enabled = true, icon = " " },
 		},
 		gitsigns = {
 			added = { enabled = true, icon = "+" },

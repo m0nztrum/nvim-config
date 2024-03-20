5f2c70f07c864c2589e5091cefd1c23bc9ed8ee7
delafiado@gmail.com <delafiado@gmail.com>
2024-03-20 01:00:44 +0000

    made some changes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -12,33 +12,6 @@ return {
 		},
 	},
 
-	--      wilder
-	{
-		"gelguy/wilder.nvim",
-		enabled = false,
-		config = function()
-			local wilder = require("wilder")
-			wilder.setup({
-				modes = { ":", "/", "?" },
-			})
-
-			wilder.set_option(
-				"renderer",
-				wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
-					highlighter = wilder.basic_highlighter(),
-					left = { " ", wilder.popupmenu_devicons() },
-					right = { " ", wilder.popupmenu_scrollbar() },
-					-- 'single', 'double', 'rounded' or 'solid'
-					max_height = "50%",
-					min_height = "0",
-					pumblend = 0,
-					border = "rounded",
-					prompt_position = "bottom",
-					reverse = 0,
-				}))
-			)
-		end,
-	},
 
 	{ "romainl/vim-cool" },
 

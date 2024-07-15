6c7abb4b9c00012040a1819c8a4e86afe1c876c8
m0nztrum <delafiado@gmail.com>
2024-07-15 17:58:57 +0000

    moved to core

--- lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- highlight yanked text for 500ms after yanking
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 500,
-		})
-	end,
-})

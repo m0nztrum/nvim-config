0addf5ab3443f36c6db7e5e1f4efcd78bb7b1f14
m0nztrum <delafiado@gmail.com>
2024-10-07 22:09:02 +0000

    misc

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,10 +1,3 @@
 return {
     "xiyaowong/nvim-transparent",
-    config = function()
-        require("transparent").clear_prefix("NeoTree")
-        require("transparent").clear_prefix("WhichKey")
-        require("transparent").clear_prefix("Noice")
-        require("transparent").clear_prefix("Notify")
-        require("transparent").clear_prefix("Telescope")
-    end,
 }
